c0554a1fc311abaffe727c58339d4aa04678d0d1
Utsav Sharma <utsav19913@gmail.com>
2024-02-26 19:24:06 +0530

    Added Task Compnent and styling

--- src/components/board/task/Task.jsx
+++ src/components/board/task/Task.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import styles from './Task.module.css'
+import React from "react";
+import styles from "./Task.module.css";
 function Task() {
   return (
-    <div>Task</div>
-  )
+    <div className={styles.container}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id="topping"
+        name="topping"
+        value="Paneer"
+      ></input>
+      <input
+        className={styles.textarea}
+        type="textarea"
+        id="name"
+        name="name"
+      ></input>
+    </div>
+  );
 }
 
-export default Task
+export default Task;
--- src/components/board/taskCard/TaskCard.jsx
+++ src/components/board/taskCard/TaskCard.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
-import styles from './TaskCard.module.css'
-import Task from '../task/Task'
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
-import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
-import { grey } from '@mui/material/colors';
+import React from "react";
+import styles from "./TaskCard.module.css";
+import Task from "../task/Task";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import { grey } from "@mui/material/colors";
 function TaskCard() {
   return (
     <div className={styles.container}>
-        <div className={styles.priority_container}>
-            <div className={styles.priority}> High Priority</div>
-            <button className={styles.collapseButton}><MoreHorizRoundedIcon/></button>
+      <div className={styles.priority_container}>
+        <div className={styles.priority}> High Priority</div>
+        <button className={styles.collapseButton}>
+          <MoreHorizRoundedIcon />
+        </button>
+      </div>
+      <h1 className={styles.taskTitle}>Hero Section</h1>
+      <div className={styles.checklistHeadingContainer}>
+        <p>Checklist (1/3)</p>
+        <div className={styles.taskCollapseButton}>
+          {" "}
+          <KeyboardArrowDownRoundedIcon sx={{ color: "grey" }} />
         </div>
-        <h1 className={styles.taskTitle}>Hero Section</h1>
-        <div className={styles.checklistHeadingContainer}>
-            <p>Checklist (1/3)</p>
-            <div className={styles.taskCollapseButton}> <KeyboardArrowDownRoundedIcon sx={{ color: "grey" }}/></div>
-            
+      </div>
+      <div className={styles.tasks}>
+        <Task></Task>
+        <Task></Task>
+      </div>
+      <div className={styles.footer}>
+        <button> select date</button>
+        <div className={styles.footerInside}>
+          {/*add a check to not show current status button */}
+          <button> backlog</button>
+          <button>to do</button>
+          <button>in progress</button>
+          {/* <button>done </button> */}
         </div>
-        <div className={styles.tasks}> <Task></Task>
-        <Task></Task></div>
-        <div className={styles.footer}>
-            <button> select date</button>
-            <div className={styles.footerInside}>
-                {/*add a check to not show current status button */}
-                <button> backlog</button>
-                <button>to do</button>
-                <button>in progress</button>
-                {/* <button>done </button> */}
-            </div>
-        </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default TaskCard
+export default TaskCard;
